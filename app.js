a37df6f7a6ede57fbc11dd2b3e9d50777abec03d
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-04-11 21:03:06 -0700

    - Pulled in Bootstrap CDN - Improved layout.hbs - Favicon.ico working properly now - Changed route 'users' to 'snacks'

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var snacks = require('./routes/snacks');
 
 var app = express();
 
@@ -14,14 +14,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'images', 'beer.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/snacks', snacks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
