d569f58aa719ccb9048ec3a95fdd7c86ebe4800a
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-04-12 11:57:08 -0700

    POST OPERATIONAL
    - 'new' form created with cheersing glasses and shit
    - Post route created and recirects to '/snacks'

--- routes/snacks.js
+++ routes/snacks.js
@@ -9,11 +9,25 @@ router.get('/', function(req, res, next) {
   })
 })
 
+router.get('/new', function(req, res, next) {
+  res.render('new')
+})
+
 router.get('/:id', function (req, res, next) {
   var id = req.params.id
   knex('snacks').where({ id }).then(function (oneSnack) {
     console.log(oneSnack);
     res.render('view-one', {oneSnack})
+  })
+})
+
+router.post('/', function (req, res, next) {
+  var snack = {
+    name, img_url, rating, review
+  } = req.body
+  console.log(snack);
+  knex('snacks').insert(snack).then(function() {
+    res.redirect('/snacks')
   })
 })
 
